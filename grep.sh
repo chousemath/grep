713c3962d66c07d32e9249e04b8252209c8e6de8
Joseph Sungpil Choi <chousemath@gmail.com>
2019-07-11 13:48:17 +0900

    Update

--- grep.sh
+++ grep.sh
@@ -16,36 +16,36 @@ $ grep "thank you" s1_e1.txt s1_e2.txt s1_e3.txt
 $ grep funny *.txt*
 
 # Redirecting output to a file
-$ grep funny *.txt* > occurrences_of_funny.txt
+$ grep funny *.txt* > results/lines_of_funny.txt
 
 # Case insensitive search for a pattern
-$ grep -i yes *.txt* > occurrences_of_yes.txt
+$ grep -i yes *.txt* > results/lines_of_yes.txt
 
 # Searching a pattern in sub-directories
-$ grep -i always s1/*.txt* > occurrences_of_always.txt
+$ grep -i always s1/*.txt* > results/lines_of_always.txt
 
 # Display line number for search pattern
-$ grep -i -n forever s1/*.txt* > occurrences_of_forever.txt
+$ grep -i -n forever s1/*.txt* > results/lines_of_forever.txt
 
 # Print lines starting with the pattern
 # Get all of Ross' lines in Season 1 of Friends
-$ grep -n ^Ross s1/*.txt* > occurrences_of_ross.txt
-$ grep -n ^Monica s1/*.txt* > occurrences_of_monica.txt
+$ grep -n ^Ross s1/*.txt* > results/lines_of_ross.txt
+$ grep -n ^Monica s1/*.txt* > results/lines_of_monica.txt
 
 # Print lines ending with the pattern
-$ grep -n ?$ s1/*.txt* > occurrences_of_questions.txt
+$ grep -n ?$ s1/*.txt* > results/lines_of_questions.txt
 
 # Print all lines excluding the matched ones
-$ grep -i -v chandler s1/*.txt* > occurrences_without_chandler.txt
+$ grep -i -v chandler s1/*.txt* > results/lines_without_chandler.txt
 
 # Find the words that contains any character
-$ grep -n .! s1/*.txt* > occurrences_with_exclamation.txt
+$ grep -n .! s1/*.txt* > results/lines_with_exclamation.txt
 
 # Combining grep with another command
-curl https://fangj.github.io/friends/season/0201.html | grep -i chandler > occurrences_with_chandler.html
+curl https://fangj.github.io/friends/season/0201.html | grep -i chandler > results/lines_with_chandler.html
 
 # Combined word search using extended words
-$ grep -Ew 'Monica:|Chandler:|Monica and|Chandler and' s1/*.txt* | grep -v "Scene" > occurences_with_monica_or_chandler.txt
+$ grep -Ew 'Monica:|Chandler:' s1/*.txt* > results/lines_with_monica_or_chandler.txt
 
 # Counting the number of occurrences acroos all text files
 $ grep -c -i friend s1/*.txt*
